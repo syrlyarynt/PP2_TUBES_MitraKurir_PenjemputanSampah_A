41027c2fb43d99e5ab7b8041689dd8e0dd927c54
syrlyarynt <aryantisyerly9@gmail.com>
2024-12-28 21:59:36 +0700

    memperbaiki conflict

--- src/main/java/kelompok4/praktikumpemrograman2/MainFrame.java
+++ src/main/java/kelompok4/praktikumpemrograman2/MainFrame.java
@@ -6,12 +6,13 @@ import javax.swing.JTabbedPane;
 
 import com.formdev.flatlaf.intellijthemes.FlatGrayIJTheme;
 
-import kelompok4.praktikumpemrograman2.view.ContohDua;
-import kelompok4.praktikumpemrograman2.view.ContohEmpat;
-import kelompok4.praktikumpemrograman2.view.ContohSatu;
-import kelompok4.praktikumpemrograman2.view.ContohTiga;
-import kelompok4.praktikumpemrograman2.view.HistoryPenjemputan;
+import kelompok4.praktikumpemrograman2.view.JenisDanKategori;
+import kelompok4.praktikumpemrograman2.view.MelihatPermintaanView;
+import kelompok4.praktikumpemrograman2.view.MenerimaPermintaanView;
+import kelompok4.praktikumpemrograman2.view.LokasiDropbox;
 import kelompok4.praktikumpemrograman2.view.TotalSampah;
+import kelompok4.praktikumpemrograman2.view.HistoryPenjemputan;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -29,19 +30,13 @@ public class MainFrame {
         JTabbedPane tabbedPane = new JTabbedPane();
 
         // Tabs
-<<<<<<< HEAD
+
         tabbedPane.addTab("Jenis dan Kategori", new JenisDanKategori().getPanel());
-        tabbedPane.addTab("Permintaan Penjemputan", new ContohDua().getPanel());
-        tabbedPane.addTab("Menerima Prmintaan Penjemputan", new ContohTiga().getPanel());
-        tabbedPane.addTab("Lokasi Dropbox", new LokasiDropbox().getPanel());
-=======
-        tabbedPane.addTab("Jenis dan Kategori", new ContohSatu().getPanel());
         tabbedPane.addTab("Permintaan Penjemputan", new MelihatPermintaanView().getPanel());
         tabbedPane.addTab("Menerima Permintaan Penjemputan", new MenerimaPermintaanView().getPanel());
-        tabbedPane.addTab("Lokasi Dropbox", new ContohEmpat().getPanel());
->>>>>>> dev_Zuhdi
-        tabbedPane.addTab("Total Sampah", new ContohLima().getPanel());
-        tabbedPane.addTab("History Penjemputan", new ContohEnam().getPanel());
+        tabbedPane.addTab("Lokasi Dropbox", new LokasiDropbox().getPanel());
+        tabbedPane.addTab("Total Sampah", new TotalSampah().getPanel());
+        tabbedPane.addTab("History Penjemputan", new HistoryPenjemputan().getPanel());
         
         // Masukin ke frame
         frame.add(tabbedPane);
