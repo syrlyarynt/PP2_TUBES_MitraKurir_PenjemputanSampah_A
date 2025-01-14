8ad458d410309e092ecab425985d6cf6d0d0c9e6
Lisvindanu <Lisvindanu015@gmail.com>
2025-01-14 07:45:40 +0700

    Merubah nama file agar menyesuaikan dengan yang lain untuk HistoryPenjemputan dan JenisDanKategori menjadi HistoryPenjemputanView dan JenisDanKategoriView

--- src/main/java/kelompok4/praktikumpemrograman2/services/KurirService.java
+++ src/main/java/kelompok4/praktikumpemrograman2/services/KurirService.java
@@ -1,11 +1,57 @@
+//package kelompok4.praktikumpemrograman2.services;
+//
+//import kelompok4.praktikumpemrograman2.model.Kurir;
+//import kelompok4.praktikumpemrograman2.model.KurirMapper;
+//import org.apache.ibatis.session.SqlSession;
+//import java.util.List;
+//
+//public class KurirService {
+//    private final SqlSession sqlSession;
+//    private final KurirMapper mapper;
+//
+//    public KurirService(SqlSession sqlSession) {
+//        this.sqlSession = sqlSession;
+//        this.mapper = sqlSession.getMapper(KurirMapper.class);
+//    }
+//
+//    public List<Kurir> getAllKurir() {
+//        return mapper.getAllKurir();
+//    }
+//
+//    public Kurir getKurirById(int id) {
+//        Kurir kurir = mapper.getKurirById(id);
+//        System.out.println("Service: Kurir = " + kurir); // Debugging
+//        return kurir;
+//    }
+//
+//    public void insertKurir(Kurir kurir) {
+//        mapper.insertKurir(kurir);
+//        sqlSession.commit();
+//    }
+//
+//    public void updateKurir(Kurir kurir) {
+//        mapper.updateKurir(kurir);
+//        sqlSession.commit();
+//    }
+//
+//    public void deleteKurir(int id) {
+//        mapper.deleteKurir(id);
+//        sqlSession.commit();
+//    }
+//}
+
 package kelompok4.praktikumpemrograman2.services;
 
 import kelompok4.praktikumpemrograman2.model.Kurir;
 import kelompok4.praktikumpemrograman2.model.KurirMapper;
+import kelompok4.praktikumpemrograman2.utils.DataCache;
 import org.apache.ibatis.session.SqlSession;
 import java.util.List;
+import java.util.Optional;
 
 public class KurirService {
+    private static final String CACHE_KEY_ALL = "kurir_all";
+    private static final String CACHE_KEY_PREFIX = "kurir_";
     private final SqlSession sqlSession;
     private final KurirMapper mapper;
 
@@ -15,27 +61,67 @@ public class KurirService {
     }
 
     public List<Kurir> getAllKurir() {
-        return mapper.getAllKurir();
+        Optional<Object> cached = DataCache.get(CACHE_KEY_ALL);
+        if (cached.isPresent()) {
+            return (List<Kurir>) cached.get();
+        }
+
+        List<Kurir> kurirList = mapper.getAllKurir();
+        DataCache.put(CACHE_KEY_ALL, kurirList);
+        return kurirList;
     }
 
     public Kurir getKurirById(int id) {
+        String cacheKey = CACHE_KEY_PREFIX + id;
+        Optional<Object> cached = DataCache.get(cacheKey);
+        if (cached.isPresent()) {
+            return (Kurir) cached.get();
+        }
+
         Kurir kurir = mapper.getKurirById(id);
-        System.out.println("Service: Kurir = " + kurir); // Debugging
+        System.out.println("Service: Kurir = " + kurir);
+        if (kurir != null) {
+            DataCache.put(cacheKey, kurir);
+        }
         return kurir;
     }
 
     public void insertKurir(Kurir kurir) {
-        mapper.insertKurir(kurir);
-        sqlSession.commit();
+        try {
+            mapper.insertKurir(kurir);
+            sqlSession.commit();
+            invalidateCache();
+        } catch (Exception e) {
+            sqlSession.rollback();
+            throw e;
+        }
     }
 
     public void updateKurir(Kurir kurir) {
-        mapper.updateKurir(kurir);
-        sqlSession.commit();
+        try {
+            mapper.updateKurir(kurir);
+            sqlSession.commit();
+            invalidateCache();
+            DataCache.put(CACHE_KEY_PREFIX + kurir.getId(), kurir);
+        } catch (Exception e) {
+            sqlSession.rollback();
+            throw e;
+        }
     }
 
     public void deleteKurir(int id) {
-        mapper.deleteKurir(id);
-        sqlSession.commit();
+        try {
+            mapper.deleteKurir(id);
+            sqlSession.commit();
+            invalidateCache();
+        } catch (Exception e) {
+            sqlSession.rollback();
+            throw e;
+        }
+    }
+
+    private void invalidateCache() {
+        DataCache.invalidateKeysByPrefix(CACHE_KEY_PREFIX);
+        DataCache.remove(CACHE_KEY_ALL);
     }
 }
--- src/main/java/kelompok4/praktikumpemrograman2/services/PermintaanPenjemputanService.java
+++ src/main/java/kelompok4/praktikumpemrograman2/services/PermintaanPenjemputanService.java
@@ -1,18 +1,138 @@
+//package kelompok4.praktikumpemrograman2.services;
+//
+//import kelompok4.praktikumpemrograman2.model.JenisKategori;
+//import kelompok4.praktikumpemrograman2.model.LokasiDropbox;
+//import kelompok4.praktikumpemrograman2.model.PermintaanPenjemputan;
+//import kelompok4.praktikumpemrograman2.model.PermintaanPenjemputanMapper;
+//import org.apache.ibatis.exceptions.PersistenceException;
+//import org.apache.ibatis.session.SqlSession;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import java.util.List;
+//
+//public class PermintaanPenjemputanService {
+//    private static final Logger logger = LoggerFactory.getLogger(PermintaanPenjemputanService.class);
+//    private final PermintaanPenjemputanMapper mapper;
+//    private final LokasiDropboxService dropboxService;
+//    private final SqlSession sqlSession;
+//
+//    public PermintaanPenjemputanService(SqlSession sqlSession) {
+//        logger.debug("Initializing PermintaanPenjemputanService with SqlSession: {}", sqlSession);
+//        this.sqlSession = sqlSession;
+//        this.mapper = sqlSession.getMapper(PermintaanPenjemputanMapper.class);
+//        this.dropboxService = new LokasiDropboxService(sqlSession);
+//    }
+//
+//    public void createPermintaan(PermintaanPenjemputan permintaan) {
+//        try {
+//            logger.info("Mencoba insert permintaan: {}", permintaan);
+//            mapper.insertPermintaan(permintaan);
+//            sqlSession.commit();
+//            logger.info("Permintaan berhasil dibuat dengan ID: {}", permintaan.getIdPermintaan());
+//        } catch (Exception e) {
+//            logger.error("Gagal membuat permintaan: {}", e.getMessage(), e);
+//            sqlSession.rollback();
+//            throw new RuntimeException("Insert gagal: " + e.getMessage());
+//        }
+//    }
+//
+//    public boolean isKategoriExists(int id) {
+//        return mapper.isKategoriExists(id) > 0;
+//    }
+//
+//    public boolean isDropboxExists(int id) {
+//        return mapper.isDropboxExists(id) > 0;
+//    }
+//
+//
+//
+//    public List<PermintaanPenjemputan> getAllPermintaan() {
+//        System.out.println("=== PermintaanPenjemputanController.getAllPermintaan START ===");
+//        sqlSession.clearCache();
+//        List<PermintaanPenjemputan> permintaanList = mapper.getAllPermintaan();
+//
+//        if (permintaanList == null || permintaanList.isEmpty()) {
+//            System.out.println("Tidak ada data permintaan ditemukan!");
+//        } else {
+//            System.out.println("Retrieved " + permintaanList.size() + " permintaan records.");
+//            for (PermintaanPenjemputan permintaan : permintaanList) {
+//                System.out.println("Permintaan ID: " + permintaan.getIdPermintaan() + ", Nama Pelanggan: " + permintaan.getNamaPelanggan() + ", Alamat: " + permintaan.getAlamat());
+//            }
+//        }
+//
+//        System.out.println("=== PermintaanPenjemputanController.getAllPermintaan END ===");
+//        return permintaanList;
+//    }
+//
+//
+//    public PermintaanPenjemputan getPermintaanById(int id) {
+//        logger.debug("Fetching permintaan with ID: {}", id);
+//        PermintaanPenjemputan permintaan = mapper.getPermintaanById(id);
+//        if (permintaan == null) {
+//            logger.warn("No permintaan found with ID: {}", id);
+//        }
+//        return permintaan;
+//    }
+//
+//    public void updatePermintaan(PermintaanPenjemputan permintaan) {
+//        logger.info("Attempting to update permintaan: {}", permintaan);
+//        try {
+//            mapper.updatePermintaan(permintaan);
+//            sqlSession.commit();
+//            logger.info("Successfully updated permintaan with ID: {}", permintaan.getIdPermintaan());
+//        } catch (Exception e) {
+//            logger.error("Failed to update permintaan: {}", permintaan, e);
+//            sqlSession.rollback();
+//            throw e;
+//        }
+//    }
+//
+//    public void deletePermintaan(int id) {
+//        logger.info("Attempting to delete permintaan with ID: {}", id);
+//        try {
+//            mapper.deletePermintaan(id);
+//            sqlSession.commit();
+//            logger.info("Successfully deleted permintaan with ID: {}", id);
+//        } catch (Exception e) {
+//            logger.error("Failed to delete permintaan with ID: {}", id, e);
+//            sqlSession.rollback();
+//            throw e;
+//        }
+//    }
+//
+//    public List<JenisKategori> getAllKategoriSampah() {
+//        logger.debug("Fetching all kategori sampah");
+//        List<JenisKategori> kategoriList = mapper.getAllKategoriSampah();
+//        logger.debug("Retrieved {} kategori records", kategoriList.size());
+//        return kategoriList;
+//    }
+//
+//    public List<LokasiDropbox> getAllDropbox() {
+//        logger.debug("Fetching all dropbox locations");
+//        List<LokasiDropbox> dropboxList = dropboxService.getAllDropbox();
+//        logger.debug("Retrieved {} dropbox records", dropboxList.size());
+//        return dropboxList;
+//    }
+//}
+
 package kelompok4.praktikumpemrograman2.services;
 
-import kelompok4.praktikumpemrograman2.model.JenisKategori;
-import kelompok4.praktikumpemrograman2.model.LokasiDropbox;
-import kelompok4.praktikumpemrograman2.model.PermintaanPenjemputan;
-import kelompok4.praktikumpemrograman2.model.PermintaanPenjemputanMapper;
-import org.apache.ibatis.exceptions.PersistenceException;
+import kelompok4.praktikumpemrograman2.model.*;
+import kelompok4.praktikumpemrograman2.utils.DataCache;
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.List;
+import java.util.Optional;
 
 public class PermintaanPenjemputanService {
     private static final Logger logger = LoggerFactory.getLogger(PermintaanPenjemputanService.class);
+    private static final String CACHE_KEY_ALL = "permintaan_all";
+    private static final String CACHE_KEY_PREFIX = "permintaan_";
+    private static final String CACHE_KEY_KATEGORI = "kategori_all";
+    private static final String CACHE_KEY_DROPBOX = "dropbox_all";
+
     private final PermintaanPenjemputanMapper mapper;
     private final LokasiDropboxService dropboxService;
     private final SqlSession sqlSession;
@@ -30,6 +150,7 @@ public class PermintaanPenjemputanService {
             mapper.insertPermintaan(permintaan);
             sqlSession.commit();
             logger.info("Permintaan berhasil dibuat dengan ID: {}", permintaan.getIdPermintaan());
+            invalidateCache();
         } catch (Exception e) {
             logger.error("Gagal membuat permintaan: {}", e.getMessage(), e);
             sqlSession.rollback();
@@ -37,39 +158,43 @@ public class PermintaanPenjemputanService {
         }
     }
 
-    public boolean isKategoriExists(int id) {
-        return mapper.isKategoriExists(id) > 0;
-    }
-
-    public boolean isDropboxExists(int id) {
-        return mapper.isDropboxExists(id) > 0;
-    }
-
-    
-
     public List<PermintaanPenjemputan> getAllPermintaan() {
+        Optional<Object> cached = DataCache.get(CACHE_KEY_ALL);
+        if (cached.isPresent()) {
+            return (List<PermintaanPenjemputan>) cached.get();
+        }
+
         System.out.println("=== PermintaanPenjemputanController.getAllPermintaan START ===");
-        sqlSession.clearCache();
         List<PermintaanPenjemputan> permintaanList = mapper.getAllPermintaan();
-        
-        if (permintaanList == null || permintaanList.isEmpty()) {
-            System.out.println("Tidak ada data permintaan ditemukan!");
-        } else {
+
+        if (permintaanList != null && !permintaanList.isEmpty()) {
+            DataCache.put(CACHE_KEY_ALL, permintaanList);
             System.out.println("Retrieved " + permintaanList.size() + " permintaan records.");
             for (PermintaanPenjemputan permintaan : permintaanList) {
-                System.out.println("Permintaan ID: " + permintaan.getIdPermintaan() + ", Nama Pelanggan: " + permintaan.getNamaPelanggan() + ", Alamat: " + permintaan.getAlamat());
+                System.out.println("Permintaan ID: " + permintaan.getIdPermintaan() +
+                        ", Nama Pelanggan: " + permintaan.getNamaPelanggan() +
+                        ", Alamat: " + permintaan.getAlamat());
             }
+        } else {
+            System.out.println("Tidak ada data permintaan ditemukan!");
         }
-        
+
         System.out.println("=== PermintaanPenjemputanController.getAllPermintaan END ===");
         return permintaanList;
     }
-    
 
     public PermintaanPenjemputan getPermintaanById(int id) {
+        String cacheKey = CACHE_KEY_PREFIX + id;
+        Optional<Object> cached = DataCache.get(cacheKey);
+        if (cached.isPresent()) {
+            return (PermintaanPenjemputan) cached.get();
+        }
+
         logger.debug("Fetching permintaan with ID: {}", id);
         PermintaanPenjemputan permintaan = mapper.getPermintaanById(id);
-        if (permintaan == null) {
+        if (permintaan != null) {
+            DataCache.put(cacheKey, permintaan);
+        } else {
             logger.warn("No permintaan found with ID: {}", id);
         }
         return permintaan;
@@ -81,6 +206,8 @@ public class PermintaanPenjemputanService {
             mapper.updatePermintaan(permintaan);
             sqlSession.commit();
             logger.info("Successfully updated permintaan with ID: {}", permintaan.getIdPermintaan());
+            invalidateCache();
+            DataCache.put(CACHE_KEY_PREFIX + permintaan.getIdPermintaan(), permintaan);
         } catch (Exception e) {
             logger.error("Failed to update permintaan: {}", permintaan, e);
             sqlSession.rollback();
@@ -94,6 +221,7 @@ public class PermintaanPenjemputanService {
             mapper.deletePermintaan(id);
             sqlSession.commit();
             logger.info("Successfully deleted permintaan with ID: {}", id);
+            invalidateCache();
         } catch (Exception e) {
             logger.error("Failed to delete permintaan with ID: {}", id, e);
             sqlSession.rollback();
@@ -102,16 +230,43 @@ public class PermintaanPenjemputanService {
     }
 
     public List<JenisKategori> getAllKategoriSampah() {
+        Optional<Object> cached = DataCache.get(CACHE_KEY_KATEGORI);
+        if (cached.isPresent()) {
+            return (List<JenisKategori>) cached.get();
+        }
+
         logger.debug("Fetching all kategori sampah");
         List<JenisKategori> kategoriList = mapper.getAllKategoriSampah();
         logger.debug("Retrieved {} kategori records", kategoriList.size());
+        DataCache.put(CACHE_KEY_KATEGORI, kategoriList);
         return kategoriList;
     }
 
     public List<LokasiDropbox> getAllDropbox() {
+        Optional<Object> cached = DataCache.get(CACHE_KEY_DROPBOX);
+        if (cached.isPresent()) {
+            return (List<LokasiDropbox>) cached.get();
+        }
+
         logger.debug("Fetching all dropbox locations");
         List<LokasiDropbox> dropboxList = dropboxService.getAllDropbox();
         logger.debug("Retrieved {} dropbox records", dropboxList.size());
+        DataCache.put(CACHE_KEY_DROPBOX, dropboxList);
         return dropboxList;
+    }
+
+    public boolean isKategoriExists(int id) {
+        return mapper.isKategoriExists(id) > 0;
+    }
+
+    public boolean isDropboxExists(int id) {
+        return mapper.isDropboxExists(id) > 0;
+    }
+
+    private void invalidateCache() {
+        DataCache.invalidateKeysByPrefix(CACHE_KEY_PREFIX);
+        DataCache.remove(CACHE_KEY_ALL);
+        DataCache.remove(CACHE_KEY_KATEGORI);
+        DataCache.remove(CACHE_KEY_DROPBOX);
     }
 }
